9ccadc5b45619b51fe54b77e0d2326955ee35c41
NathanielJS1541 <nathaniel.struselis@hotmail.co.uk>
2019-04-08 17:22:34 +0100

    Added error checking to the initialise() function and to the updateData() function. They also now use SMBus commands to hopefully be more reliable.

--- MPU6050.cpp
+++ MPU6050.cpp
@@ -8,28 +8,40 @@
  * MPU6050 IMU. The registers are based off the addresses found at invensense:
  * https://www.invensense.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf. This is
  * based on the Pi2c library from https://github.com/JohnnySheppard/Pi2c.
+ * Additional I2C documentation was found at
+ * https://www.kernel.org/doc/Documentation/i2c/dev-interface.
  * --------------------------------------------------------------------------------------------
  */
 
 #include "MPU6050.h" // Include definitions and declarations within the header file
 
+#include <iostream>  // Used for data output
+
 // Used for the I2C interface
 #include <linux/i2c-dev.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>    // For O_RDWR
-#include <unistd.h>   // For open()
+#include <sys/ioctl.h>     // For ioctl()
+#include <fcntl.h>         // For O_RDWR
+#include <unistd.h>        // For open()
 
 // ----------------------------- Special class member definitions -----------------------------
 // Default constructor
 MPU6050::MPU6050(){
+    // If the address for the device is not specified, use the default address.
     address = MPU_DEFAULT_I2C_ADDR;
 
+    snprintf(fileName, 10, "/dev/i2c-%d", 1); // The I2C interface is 1 unless it is a rev0 Pi
+
+    // Initialise the I2C interface
     i2cHandle = open(fileName, O_RDWR);
     if (i2cHandle < 0) {
-		std::cout << std::endl << "Can't open I2C BUS" << std::endl; //If there's an error opening this, then display it.
-	}
-	if (ioctl(i2cHandle, I2C_SLAVE, address) < 0) { //Using ioctl set the i2c device to talk to address in the "addr" variable.
-		std::cout << std::endl << "Can't set the I2C address for the slave device" << std::endl; //Display error setting the address for the slave.
+        std::cout << std::endl << "Couldn't open the I2C Bus. Please ensure the I2C interface is enabled." << std::endl;
+        exit(I2C_BUS_INIT_ERROR);
+    }
+
+    // Set the slave address for the device
+	if (ioctl(i2cHandle, I2C_SLAVE, address) < 0) {
+		std::cout << std::endl << "The I2C Device couldn't be assigned a slave address." << std::endl;
+		exit(I2C_SET_SLAVE_ADDR_ERR);
 	}
 
     gyroX = 0;
@@ -39,7 +51,9 @@ MPU6050::MPU6050(){
     accelY = 0;
     accelZ = 0;
     temperature = 0;
-    initialise(); // Set the registers for the MPU
+
+    // Set the registers for the MPU
+    initialise();
 }
 
 MPU6050::MPU6050(const MPU6050& M){
@@ -81,50 +95,81 @@ MPU6050& MPU6050::operator=(const MPU6050& M){
 // ---------------------------------- Data Access Functions -----------------------------------
 void MPU6050::updateData(){
     char rawData[2];
-    char requestRegister[1];
+    __u8 requestRegister;
+    __u32 returnedData;
 
     // ------------ Get Gyro Data ------------
     // X Axis
-    requestRegister[0] = MPU_GYRO_X1;
-    write(i2cHandle, requestRegister, 1);
-    read(i2cHandle, rawData, 2);
-    gyroX = float(rawData[0] << 8 | rawData[1]);
+    requestRegister = MPU_GYRO_X1;
+    returnedData = i2c_smbus_read_word_data(i2cHandle, requestRegister);
+    if(returnedData < 0){
+        std::cout << std::endl << "Error accessing Gyro X axis data." << std::endl;
+    }
+    else{
+        gyroX = float(returnedData);
+    }
+
     // Y Axis
-    requestRegister[0] = MPU_GYRO_Y1;
-    write(i2cHandle, requestRegister, 1);
-    read(i2cHandle, rawData, 2);
-    gyroY = float(rawData[0] << 8 | rawData[1]);
+    requestRegister = MPU_GYRO_Y1;
+    returnedData = i2c_smbus_read_word_data(i2cHandle, requestRegister);
+    if(returnedData < 0){
+        std::cout << std::endl << "Error accessing Gyro Y axis data." << std::endl;
+    }
+    else{
+        gyroY = float(returnedData);
+    }
+
     // Z Axis
-    requestRegister[0] = MPU_GYRO_Z1;
-    write(i2cHandle, requestRegister, 1);
-    read(i2cHandle, rawData, 2);
-    gyroZ = float(rawData[0] << 8 | rawData[1]);
+    requestRegister = MPU_GYRO_Z1;
+    returnedData = i2c_smbus_read_word_data(i2cHandle, requestRegister);
+    if(returnedData < 0){
+        std::cout << std::endl << "Error accessing Gyro Z axis data." << std::endl;
+    }
+    else{
+        gyroZ = float(returnedData);
+    }
     // ---------------------------------------
 
     // ------- Get Accelerometer Data --------
     // X Axis
-    requestRegister[0] = MPU_ACC_X1;
-    write(i2cHandle, requestRegister, 1);
-    read(i2cHandle, rawData, 2);
-    accelX = float(rawData[0] << 8 | rawData[1]);
+    requestRegister = MPU_ACC_X1;
+    returnedData = i2c_smbus_read_word_data(i2cHandle, requestRegister);
+    if(returnedData < 0){
+        std::cout << std::endl << "Error accessing Accel X axis data." << std::endl;
+    }
+    else{
+        accelX = float(returnedData);
+    }
     // Y Axis
-    requestRegister[0] = MPU_ACC_Y1;
-    write(i2cHandle, requestRegister, 1);
-    read(i2cHandle, rawData, 2);
-    accelY = float(rawData[0] << 8 | rawData[1]);
+    requestRegister = MPU_ACC_Y1;
+    returnedData = i2c_smbus_read_word_data(i2cHandle, requestRegister);
+    if(returnedData < 0){
+        std::cout << std::endl << "Error accessing Accel Y axis data." << std::endl;
+    }
+    else{
+        accelY = float(returnedData);
+    }
     // Z Axis
-    requestRegister[0] = MPU_ACC_Z1;
-    write(i2cHandle, requestRegister, 1);
-    read(i2cHandle, rawData, 2);
-    accelZ = float(rawData[0] << 8 | rawData[1]);
+    requestRegister = MPU_ACC_Z1;
+    returnedData = i2c_smbus_read_word_data(i2cHandle, requestRegister);
+    if(returnedData < 0){
+        std::cout << std::endl << "Error accessing Accel Z axis data." << std::endl;
+    }
+    else{
+        accelZ = float(returnedData);
+    }
     // ---------------------------------------
 
     // -------- Get Temperature Data ---------
-    requestRegister[0] = MPU_TEMP1;
-    write(i2cHandle, requestRegister, 1);
-    read(i2cHandle, rawData, 2);
-    temperature = float(rawData[0] << 8 | rawData[1]);
-    temperature = (temperature/340)+36.53; // Convert temperature to Celcius
+    requestRegister = MPU_TEMP1;
+    returnedData = i2c_smbus_read_word_data(i2cHandle, requestRegister);
+    if(returnedData < 0){
+        std::cout << std::endl << "Error accessing Temperature data." << std::endl;
+    }
+    else{
+        temperature = float(returnedData);
+        temperature = (temperature/340)+36.53; // Convert temperature to Celcius
+    }
     // ---------------------------------------
 }
 
@@ -147,22 +192,38 @@ float MPU6050::getTemp(){return temperature;}
 
 // --------------------------------- Private Class Functions ----------------------------------
 void MPU6050::initialise(){
-    char dataPacket[2]; // Data to be transmitted (2 bytes at a time)
+    __u8 deviceRegister; // The device register to access
+    __s32 returnedData;  // The data returned by the device
 
-    // Configure MPU Power mode
-    dataPacket[0] = MPU_PWR_MGMT_1;
-    dataPacket[1] = MPU_PWR_MGMT_CLK_INTERNAL_8MHZ; // Use the 8 MHz internal clock
-    write(i2cHandle, dataPacket, 1);
+    // Configure the MPU Power Mode
+    deviceRegister = MPU_PWR_MGMT_1;
+    returnedData = i2c_smbus_write_byte_data(i2cHandle, deviceRegister, MPU_PWR_MGMT_CLK_INTERNAL_8MHZ);
+    if (returnedData < 0){
+        // Not sure if any data SHOULD be returned yet...
+        std::cout << std::endl << "Data was not returned by the device when setting the power register. Potential connectivity problem?" << std::endl;
+        exit(I2C_SET_SLAVE_PWR_MODE);
+    }
 
     // Configure the Gyroscope
-    dataPacket[0] = MPU_GYRO_CONFIG;
-    dataPacket[1] = MPU_GYRO_SENS_500 << 3; // Set the sensitivity. Shifted to the right to correctly position it in the register
-    write(i2cHandle, dataPacket, 1);
+    deviceRegister = MPU_GYRO_CONFIG;
+    // Set the sensitivity. Value shifted to the right to correctly position it in the register
+    returnedData = i2c_smbus_write_byte_data(i2cHandle, deviceRegister, MPU_GYRO_SENS_500 << 3);
+    if (returnedData < 0){
+        // Not sure if any data SHOULD be returned yet...
+        std::cout << std::endl << "Data was not returned by the device when setting up the Gyro. Potential connectivity problem?" << std::endl;
+        exit(I2C_SET_GYRO_RES);
+    }
+
 
     // Configure the Accelerometer
-    dataPacket[0] = MPU_ACC_CONFIG;
-    dataPacket[1] = MPU_ACC_SENS_2 << 3; // Set the sensitivity. Used for angle sensing when stationary so doesn't need a large range
-    write(i2cHandle, dataPacket, 1);
+    deviceRegister = MPU_ACC_CONFIG;
+    // Set the sensitivity. Value shifted to the right to correctly position it in the register
+    returnedData = i2c_smbus_write_byte_data(i2cHandle, deviceRegister, MPU_ACC_SENS_2 << 3);
+    if (returnedData < 0){
+        // Not sure if any data SHOULD be returned yet...
+        std::cout << std::endl << "Data was not returned by the device when setting up the Accelerometer. Potential connectivity problem?" << std::endl;
+        exit(I2C_SET_ACCEL_RES);
+    }
 }
 // --------------------------------------------------------------------------------------------
 
