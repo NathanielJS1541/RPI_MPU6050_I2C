38db6756c24d32ea4fcd9b77e9aab847af300045
NathanielJS1541 <nathaniel.struselis@hotmail.co.uk>
2019-04-08 21:08:08 +0100

    Added James to devs.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /* ============================================================================================
  * MPU6050 Interface for Raspberry Pi - Example Code
  * ============================================================================================
- * Written by Nathaniel Struselis
+ * Written by Nathaniel Struselis & James Clarke.
  * Last Update: 05/04/2019
  * --------------------------------------------------------------------------------------------
  * This main demonstrates how the program operates and how to create/use the MPU6050 class.
